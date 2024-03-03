3cd3562164eb603c4230736e35fa916e2e3cd35a
Shenan Perera <shenan.p@azendtech.com>
2024-03-03 12:34:03 +0530

    asyndAwait

--- app.js
+++ app.js
@@ -40,3 +40,13 @@ const myPromise = new Promise((resolve, reject) => {
    }).catch((error) => {
     console.log(error);
    });
+
+   async function myFunction() {
+    try {
+    const result = await myPromise;
+    console.log(result);
+    } catch (error) {
+    console.log(error);
+    }
+   }
+   myFunction()
